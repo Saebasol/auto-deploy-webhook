4b662d217c2c659f71883b2953e7edd21b065a64
Ryu JuHeon <saidbysolo@gmail.com>
2021-07-08 00:23:11 +0900

    feat(server): add subprocess shell

--- server.py
+++ server.py
@@ -1,7 +1,22 @@
+from asyncio.subprocess import PIPE, create_subprocess_shell
+
 from sanic import Sanic
+
 
 app = Sanic("server")
 
-@app.post("deploy")
+@app.get("deploy")
 async def _deploy(request):
-    ...
+    proc = await create_subprocess_shell("ls", stdout=PIPE, stderr=PIPE)
+
+    stdout, stderr = await proc.communicate()
+    print(stdout)
+
+    print(f"['ls' exited with {proc.returncode}]")
+    if stdout:
+        print(f"[stdout]\n{stdout.decode()}")
+    if stderr:
+        print(f"[stderr]\n{stderr.decode()}")
+
+
+app.run()
